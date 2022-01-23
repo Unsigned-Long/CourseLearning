cadbd15e5d987e44ab12fa81bbcce5ca29e1375f
csl <3079625093@qq.com>
2022-01-23 18:35:17 +0800

    add output info

--- optimization/src/dichSearch.cpp
+++ optimization/src/dichSearch.cpp
@@ -24,14 +24,13 @@ using fun_type = double (*)(double);
  */
 std::pair<double, double> dichotomousSearch(const fun_type &fun, double left, double right, double stride = 1E-8, bool log = false)
 {
-    double temp_left, temp_right, center;
     while (std::abs(right - left) > 2.5 * stride)
     {
         if (log)
             std::cout << "left: " << left << ", right: " << right << std::endl;
-        center = (right + left) / 2.0;
-        temp_left = center - stride;
-        temp_right = center + stride;
+        auto center = (right + left) / 2.0;
+        auto temp_left = center - stride;
+        auto temp_right = center + stride;
         if (fun(temp_left) < fun(temp_right))
             right = temp_right;
         else
@@ -44,18 +43,16 @@ std::pair<double, double> dichotomousSearch(const fun_type &fun, double left, do
 
 int main(int argc, char const *argv[])
 {
-    auto target1 = [](double x) -> double
-    {
-        return 2.0 * x * x + 3.0 * x + 10.0;
-    };
-    auto res1 = dichotomousSearch(target1, -100.0, 500.0);
-    std::cout << "the result is [minpos: " << res1.first << ", minval: " << res1.second << "]\n";
-
-    auto target2 = [](double x) -> double
+    auto target = [](double x) -> double
     {
         return 8.0 * x * x * x - 2.0 * x * x - 7.0 * x + 3.0;
     };
-    auto res2 = dichotomousSearch(target2, 0.0, 1.0);
-    std::cout << "the result is [minpos: " << res2.first << ", minval: " << res2.second << "]\n";
+    auto res = dichotomousSearch(target, 0.0, 1.0);
+    std::cout << "the result is [minpos: " << res.first << ", minval: " << res.second << "]\n";
+
+    /**
+     * @brief output
+     * the result is [minpos: 0.629787, minval: -0.203425]
+     */
     return 0;
 }
