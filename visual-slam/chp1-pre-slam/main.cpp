693858cb3b08aaffa7fb2c428a9c19aede50f8c9
csl <3079625093@qq.com>
2022-02-14 14:58:57 +0800

    rename

--- visual-slam/chp1-pre-slam/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "helloslam.h"
-
-int main(int argc, char const *argv[]) {
-  // call 'hello_slam'
-  ns_chp1::hello_slam();
-  return 0;
-}
--- visual-slam/chp1-pre-slam/src/helloslam.cpp
+++
@@ -1,9 +0,0 @@
-#include "helloslam.h"
-
-namespace ns_chp1 {
-void hello_slam() {
-  // print "Hello, slam!"
-  std::cout << "Hello, slam!" << std::endl;
-  return;
-}
-}  // namespace ns_chp1
