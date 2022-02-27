f623c29281223de1e0856724740fde0fbc366bb7
csl <3079625093@qq.com>
2022-02-27 11:14:39 +0800

    update

---
+++ visual-slam/chp6-optimization/src/use_g2o.cpp
@@ -0,0 +1,16 @@
+#include "artwork/timer/timer.h"
+#include "eigen3/Eigen/Core"
+#include "g2o/core/base_edge.h"
+#include "g2o/core/base_unary_edge.h"
+#include "g2o/core/g2o_core_api.h"
+#include "g2o/core/optimization_algorithm_gauss_newton.h"
+#include "g2o/core/optimization_algorithm_levenberg.h"
+#include "g2o/core/optimization_algorithm_dogleg.h"
+#include "g2o/solvers/dense/linear_solver_dense.h"
+
+#include "eigen3/Eigen/Dense"
+
+int main(int argc, char const *argv[]) {
+  
+  return 0;
+}
