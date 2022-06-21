f8ef0fd5c5fddbfbb054c5d8840a8581baab89b2
csl <3079625093@qq.com>
2022-06-21 17:29:16 +0800

    add notes

--- section-inspection/src/src/circle.cpp
+++ section-inspection/src/src/circle.cpp
@@ -4,11 +4,14 @@
 
 namespace ns_section {
   Circle::Circle(const ns_geo::Point2d &p1, const ns_geo::Point2d &p2, const ns_geo::Point2d &p3) {
+    // move origin
     double x2 = p2.x - p1.x, y2 = p2.y - p1.y;
     double x3 = p3.x - p1.x, y3 = p3.y - p1.y;
     double r2 = x2 * x2 + y2 * y2, r3 = x3 * x3 + y3 * y3;
+    // compute center
     cen.x = (y3 * r2 - y2 * r3) / (2.0 * (x2 * y3 - y2 * x3)) + p1.x;
     cen.y = (x2 * r3 - x3 * r2) / (2.0 * (x2 * y3 - y2 * x3)) + p1.y;
+    // compute radius
     rad = ns_geo::distance(cen, p1);
   }
 
@@ -17,11 +20,13 @@ namespace ns_section {
   }
 
   ns_geo::Point2d Circle::nearest(const ns_geo::Point2d &p) const {
+    // compute angle
     double deltaX = p.x - cen.x, deltaY = p.y - cen.y;
     double theta = std::atan2(deltaY, deltaX);
     if (deltaY < 0.0) {
       theta += 2.0 * M_PI;
     }
+    // compute point on circle
     ns_geo::Point2d target;
     target.x = rad * std::cos(theta) + cen.x;
     target.y = rad * std::sin(theta) + cen.y;
@@ -50,8 +55,10 @@ namespace ns_section {
   }
 
   Circle Circle::fit(const ns_geo::PointSet2d &points, const ushort iter) {
+    // get init parameters
     auto cir = Circle::ransac(points);
     for (int i = 0; i != iter; ++i) {
+      // get matrix
       Eigen::Matrix3d H = Eigen::Matrix3d::Zero();
       Eigen::Vector3d g = Eigen::Vector3d::Zero();
       for (const auto &p : points) {
@@ -61,7 +68,9 @@ namespace ns_section {
         H += j * j.transpose();
         g -= j * error;
       }
+      // solve
       Eigen::Vector3d delta = H.ldlt().solve(g);
+      // update
       cir.cen.x += delta(0);
       cir.cen.y += delta(1);
       cir.rad += delta(2);
