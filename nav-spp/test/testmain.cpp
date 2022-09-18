0bf50aada297b3afd1c50d276b1de331aee0716f
csl <3079625093@qq.com>
2022-09-18 12:35:43 +0800

    add test for base convert

--- nav-spp/test/testmain.cpp
+++ nav-spp/test/testmain.cpp
@@ -1,6 +1,7 @@
 #include "test_datetime.hpp"
 #include "test_config.hpp"
 #include "test_coordinate.hpp"
+#include "test_base_convert.hpp"
 
 int main(int argc, char *argv[]) {
     ns_spp::Config::loadConfigure("../config/spp.json");
